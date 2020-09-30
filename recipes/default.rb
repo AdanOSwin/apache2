ac16e3d9707ab3ab484f8061a0488f5b490db1a0
root <root@adanvazquez2c.mylabserver.com>
2020-09-30 04:46:44 +0000

    [First] recipe commit

---
+++ recipes/default.rb
@@ -0,0 +1,14 @@
+package 'apache2' do
+ action :install
+end
+
+file '/var/www/html/index.html' do
+ action :create
+ content 'Hello World'
+end
+
+service 'apache2' do
+ action [ :enable, :start ]
+end
+
+
